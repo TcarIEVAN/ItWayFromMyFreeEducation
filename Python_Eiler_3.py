07aed9cad67a394f20b0e484feb61c68b8d6ea72
TcarIEVAN <popovqik@yandex.ru>
2022-07-08 18:29:21 +0300

    Python_Eiler_2-3
    daily python study

---
+++ Python_Eiler_3.py
@@ -0,0 +1,12 @@
+# Каков самый большой делитель числа 600851475143, являющийся простым числом?
+import math
+def issimple(a):
+    r = math.ceil(math.sqrt(a))
+    lst = []
+    for i in range(3,r):
+        if a%i == 0:
+            if issimple(i) == []:
+                lst.append(i)
+    return lst
+r = issimple(600851475143)
+print(max(r))
